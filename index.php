1f22ffc87d4cd91fc186707275711d130bc0efd5
mambax7 <mambax7@gmail.com>
2020-07-11 22:45:56 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+include("../../mainfile.php");
+$xoopsOption['template_main'] = "guitartuner.html";
+include(XOOPS_ROOT_PATH."/header.php");
+$xoopsOption['show_rblock'] = 1;
+include(XOOPS_ROOT_PATH."/footer.php");
+?>
---
+++ xoops_version.php
@@ -0,0 +1,17 @@
+<?php
+$modversion['name'] = "Guitar Tuner";
+$modversion['version'] = 2.0;
+$modversion['description'] = "Flash guitar tuner.";
+$modversion['author'] = "Ported To XOOPS by Danordesign.com";
+$modversion['credits'] = "";
+$modversion['help'] = "";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "images/guitartuner.gif";
+$modversion['dirname'] = "guitartuner";
+$modversion['hasAdmin'] = 0;
+$modversion['adminpath'] = "";
+$modversion['hasMain'] = 1;
+$modversion['templates'][1]['file'] = "guitartuner.html";
+$modversion['templates'][1]['description'] = "";
+?>
